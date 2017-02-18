12bdcbf910e1d04259ea5e019db3dc41a219cb3d
Ernestom4717 <ernesm4717@gmail.com>
2017-02-17 20:37:08 -0800

    Actualización vertice
    Contenidos cambiados con Angularjs

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('VerticeApp',[]);
+
+app.controller("TabController", function() {
+    this.tab = 1;
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+  });
