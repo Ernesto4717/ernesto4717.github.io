6565d504d59320d2c92f3c7753098ca91dd4c8d9
Ernesto Murillo <ernesm4717@gmail.com>
2017-03-24 11:33:26 -0700

    Actualización,navegacion mejorada con angular-route

--- js/app.js
+++ js/app.js
@@ -1,13 +1,41 @@
-var app = angular.module('VerticeApp',[]);
+var app = angular.module('VerticeApp',['ngRoute']);
 
-app.controller("TabController", function() {
+app.config(['$routeProvider', '$locationProvider',function($routeProvider,$locationProvider) {
+    $routeProvider
+        .when("/",{
+        templateUrl : "home.html"
+    })
+        .when("/Arquitectura",{
+        templateUrl : "Arquitectura.html"
+    })
+        .when("/Bioingenieria",{
+        templateUrl : "Bioingenieria.html"
+    })
+        .when("/Civil",{
+        templateUrl : "Civil.html"
+    })
+        .when("/Industrial",{
+        templateUrl : "Industrial.html"
+    })
+        .when("/Registro",{
+        templateUrl : "Registro.html"
+    });
+    $locationProvider.html5Mode(true);
+}]);
+
+app.controller('navController', function($route) {
+    this.$route = $route;
+});
+
+app.controller('TabController', function() {
+    console.log("unexpected here");
     this.tab = 1;
     this.isSet = function(checkTab) {
-      return this.tab === checkTab;
+        return this.tab === checkTab;
     };
 
     this.setTab = function(setTab) {
-      this.tab = setTab;
+        this.tab = setTab;
         (window).scrollTo(0,0);
     };
-  });
+});
