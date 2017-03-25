1995b99c2621d83b1edd1fa42a584e0da7262aa9
Ernestom4717 <ernesm4717@gmail.com>
2017-03-25 11:25:57 -0800

    Modificaciones minimas y agregue calculo de tiempo faltante

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
-var app = angular.module('VerticeApp',['ngRoute']);
+var app = angular.module('VerticeApp', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',function($routeProvider,$locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
-        .when("/",{
+        .when("/", {
         templateUrl : "home.html"
     })
-        .when("/Arquitectura",{
+        .when("/Arquitectura", {
         templateUrl : "Arquitectura.html"
     })
-        .when("/Bioingenieria",{
+        .when("/Bioingenieria", {
         templateUrl : "Bioingenieria.html"
     })
-        .when("/Civil",{
+        .when("/Civil", {
         templateUrl : "Civil.html"
     })
-        .when("/Industrial",{
+        .when("/Industrial", {
         templateUrl : "Industrial.html"
     })
-        .when("/Registro",{
+        .when("/Registro", {
         templateUrl : "Registro.html"
     });
     $locationProvider.html5Mode(true);
 }]);
+
+app.controller('dateController', function() {
+    var fecha = new Date();
+    var fechaCongreso = new Date("April 26, 2017 00:13:00");
+    var diferencia=(fechaCongreso-fecha)/1000
+        this.dias=Math.floor(diferencia/86400)
+        diferencia=diferencia-(86400*this.dias)
+        this.horas=Math.floor(diferencia/3600)
+        diferencia=diferencia-(3600*this.horas)
+        this.minutos=Math.floor(diferencia/60)
+    console.log("Tiempo restante: " + this.dias +":" + this.horas + ":" + this.minutos);
+});
 
 app.controller('navController', function($route) {
     this.$route = $route;
--- js/main.js
+++ js/main.js
@@ -17,27 +17,27 @@ $('#toggle-navbar').click(function(){
  * Change Navbar color while scrolling
  */
 
- $(window).scroll(function(){
-     handleTopNavAnimation();
- });
+$(window).scroll(function(){
+    handleTopNavAnimation();
+});
 
- $(window).load(function(){
-     handleTopNavAnimation();
- });
+$(window).load(function(){
+    handleTopNavAnimation();
+});
 
- function handleTopNavAnimation() {
-     var top=$(window).scrollTop();
+function handleTopNavAnimation() {
+    var top=$(window).scrollTop();
 
-     if(top>10){
-      $('#site-nav').addClass('navbar-solid'); 
-  }
-  else{
-      $('#site-nav').removeClass('navbar-solid'); 
-  }
+    if(top>10){
+        $('#site-nav').addClass('navbar-solid');
+    }
+    else{
+        $('#site-nav').removeClass('navbar-solid');
+    }
 }
 
 /*
  * SmoothScroll
  */
 
- smoothScroll.init();
+smoothScroll.init();
